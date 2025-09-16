e70c2bfc7e78196254770301565edf5c141a86b9
Chinmay Daroliya <chinmaydaroliya@gmail.com>
2025-09-17 02:43:03 +0530

    created agent model

---
+++ backend/models/agent.py
@@ -0,0 +1,53 @@
+from enum import Enum
+from pydantic import BaseModel,Field,EmailStr
+from typing import Optional, List
+from datetime import datetime
+
+# Enum representing possible statuses of an agent
+class AgentStatus(str, Enum):
+    AVAILABLE = "available"
+    BUSY = "busy"
+    OFFLINE = "offline"
+
+# Request model for creating a new agent with name, email, and skills
+class AgentCreateRequest(BaseModel):
+    name: str = Field()
+    email: EmailStr = Field()
+    skills: List[str] = Field() 
+
+# Request model for updating the agent with name, email, or skills
+class AgentUpdateRequest(BaseModel):
+    name: Optional[str] = Field(None, description="Agent's full name")
+    skills: Optional[List[str]] = Field(None, description="List of agent skills")
+    max_concurrentcalls: Optional[int] = Field(None, description="maximum concurrent calls agent can handle")
+
+# Request model for updating the current status of an agent (available, busy, offline)
+class StatusUpdate(BaseModel):
+    status: AgentStatus = Field(..., description="New agent status")
+
+# Response model providing detailed information of a single agent to the frontend
+class AgentResponse(BaseModel):
+    id: str
+    name: str
+    email: str
+    status: AgentStatus
+    current_room_id: Optional[str]
+    max_concurrent_calls: int
+    skills: List[str]
+    created_at: datetime
+    updated_at: datetime
+
+    class config:
+        from_attributes: True
+
+# Response model providing a list of agents with basic info to the frontend
+class AgentListResponse(BaseModel):
+    id: str
+    name: str
+    email: str
+    status: AgentStatus
+    current_room_id: Optional[str]
+    skills: List[str]
+
+    class config:
+        from_attributes: True

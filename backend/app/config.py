b3636e26c434c38ba7a81faabed31c6527562935
Chinmay Daroliya <chinmaydaroliya@gmail.com>
2025-09-17 16:37:11 +0530

    created llm service file

--- backend/app/config.py
+++ backend/app/config.py
@@ -21,8 +21,20 @@ class Settings(BaseSettings):
 
     # LLM configuration
     OPENAI_API_KEY:str = ""
+    # GROQ_API_KEY:str = ""
+    # OPENROUTER_API_KEY = ""
+    DEFAULT_LLM_PROVIDER:str = "openai"
 
     # JWT configuration
 
+    # Transfer Configuration
+    MAX_TRANSFER_WAIT_TIME:int = 300 #5 minutes in seconds
+    MAX_ACTIVE_CALLS_PER_AGENT:int = 3
+
+    # LLM Configuration
+    MAX_SUMMARY_TOKENS:int = 500
+    SUMMARY_TEMPERATURE:float = 0.3
+
+# Create settings instance
 Settings = Settings()    
     
